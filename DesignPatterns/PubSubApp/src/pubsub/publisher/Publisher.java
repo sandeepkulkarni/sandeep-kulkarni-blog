c20a5fb4828a309dde47d87e01fc5cf19c4a199d
Sandeep Kulkarni <sandeep.kul88@gmail.com>
2016-02-19 16:08:02 -0800

    Publish-Subscribe Design Pattern
    Publish-Subscribe Design Pattern Implementation

---
+++ DesignPatterns/PubSubApp/src/pubsub/publisher/Publisher.java
@@ -0,0 +1,11 @@
+package pubsub.publisher;
+
+import pubsub.Message;
+import pubsub.service.PubSubService;
+
+public interface Publisher {	
+	/**
+	 * Publishes new message to PubSubService
+	 */
+	void publish(Message message, PubSubService pubSubService);
+}
---
+++ DesignPatterns/PubSubApp/src/pubsub/publisher/PublisherImpl.java
@@ -0,0 +1,10 @@
+package pubsub.publisher;
+import pubsub.Message;
+import pubsub.service.PubSubService;
+
+public class PublisherImpl implements Publisher {
+	//Publishes new message to PubSubService
+	public void publish(Message message, PubSubService pubSubService) {		
+		pubSubService.addMessageToQueue(message);
+	}
+}
